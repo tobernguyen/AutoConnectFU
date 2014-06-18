9d42465a4d3476fb520824e37bfd7d6a10b85799
longtober <longtober@gmail.com>
2014-06-18 11:31:35 +0700

    Update version code 1.3

--- UniDomNWC/Help.Designer.cs
+++ UniDomNWC/Help.Designer.cs
@@ -43,7 +43,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(340, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Phần mềm cài đặt mạng tự động cho sinh viên FU-HL v1.2";
+            this.label1.Text = "Phần mềm cài đặt mạng tự động cho sinh viên FU-HL v1.3";
             // 
             // label2
             // 
